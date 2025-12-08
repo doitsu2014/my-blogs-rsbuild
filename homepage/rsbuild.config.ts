fb9546592e44d83a185d015c5ac6fcaad625d3c7
Duc <thd1152015@gmail.com>
2025-12-08 16:21:37 +0700

    fix: ensure correct requiredVersion for shared dependencies and update CSS styles for homepage module

--- homepage/rsbuild.config.ts
+++ homepage/rsbuild.config.ts
@@ -16,9 +16,11 @@ export default defineConfig({
       shared: {
         react: {
           singleton: true,
+          requiredVersion: '^19.0.0'
         },
         'react-dom': {
           singleton: true,
+          requiredVersion: '^19.0.0'
         },
       },
     }),
